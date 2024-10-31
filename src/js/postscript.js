0ac9b8f5add5637a09fcbd8a3449332f092fbeb4
imresaks <imresaks@gmail.com>
2024-10-31 14:49:32 +0200

    created code for js

--- src/js/postscript.js
+++ src/js/postscript.js
@@ -1,45 +1,62 @@
-window.onload = function() {
+fetch('https://api.npoint.io/5307d739dbe03bcbb149')
+    .then(response => response.json())
+    .then(data => {
+        displayPosts(data);
+    })
+    .catch(error => console.error('Error fetching data:', error));
 
+function displayPosts(posts) {
+    const main = document.querySelector('main');
 
+    posts.forEach(post => {
+        const postElement = document.createElement('div');
+        postElement.classList.add('post');
 
-    fetch('res/json/myjson.json')
-        .then((response) => response.json())
-        .then(json => {
-            console.log(json);
-            for (const singlePost of json) {
-                const postDiv = document.createElement('div');
-                postDiv.className = 'post';
-                const posth3 = document.createElement('h3');
-                const postp = document.createElement('p');
-                //set the content of the title tag
-                posth3.innerHTML = singlePost.title;
-                //set content of p tag
-                postp.innerHTML = singlePost.body;
-                postDiv.appendChild(posth3);
-                postDiv.appendChild(postp);
+        const postHeader = document.createElement('div');
+        postHeader.classList.add('post-header');
 
-                document.body.appendChild(postDiv);
-            }
-            
-            }).catch(err => {
-                let errDiv = document.createElement("div");
-                errDiv.className = 'post';
-                errDiv.innerText = err;
-                document.body.appendChild(errDiv);
-                }).finally(() => {
-                    let footer = document.createElement("footer");
-                    date = new Date().toLocaleString()
-                    footer.innerText = date;
-                    document.body.appendChild(footer);
-                    });
-        
+        const userIcon = document.createElement('img');
+        userIcon.src = 'res/images/user-icon.png';
+        userIcon.alt = 'User Profile';
+        userIcon.classList.add('post-user-icon');
+
+        const date = document.createElement('date');
+        date.classList.add('date');
+        date.textContent = post.date;
+        const author = document.createElement('author');
+        author.classList.add('author');
+        author.textContent = post.author;
+
+        postHeader.appendChild(userIcon);
+        postHeader.appendChild(date);
+        postHeader.appendChild(author);
+
+        const description = document.createElement('p');
+        description.classList.add('description');
+        description.textContent = post.description;
+
+        postElement.appendChild(postHeader);
+
+        if (post.image) {
+            const image = document.createElement('img');
+            image.src = post.image;
+            image.alt = post.description;
+            postElement.appendChild(image);
         }
-    
 
-    
-// create loop for this:
-// create h3
-//create p
-//create div
-//add h3,p to div
-//add div to body
+        postElement.appendChild(description);
+
+        const likeButton = document.createElement('button');
+        likeButton.classList.add('like-button');
+
+        const likeIcon = document.createElement('img');
+        likeIcon.src = 'res/images/like-icon.png';
+        likeIcon.alt = 'Like Icon';
+        likeIcon.classList.add('like-icon');
+
+        likeButton.appendChild(likeIcon);
+        postElement.appendChild(likeButton);
+
+        main.appendChild(postElement);
+    });
+}
